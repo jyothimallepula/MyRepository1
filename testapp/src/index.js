f9ccdc61d936baa9ec40018a518dfb52fa398b33
unknown <abc@example.com>
2024-04-06 10:05:47 +0530

    Commit operation done

--- testapp/src/index.js
+++ testapp/src/index.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './App.css';
-import Navbar from 'react-bootstrap/Navbar';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Navbar } from 'react-bootstrap';
 
 function MyNavbar(){
-return (
-<div>
-<Navbar className="p-3" style={{ backgrounnnd:"black"}}>
-<Navbar.Brand href='#home' style={{color:"red"}}>ABC</Navbar.Brand>
-<Navbar.Brand style={{color:"blue"}}>RST</Navbar.Brand>
-</Navbar>
-</div>
-);
+  return(
+    <div>
+      <Navbar className='p-3' style={{background:"black"}}>
+        <Navbar.Brand href='#' style={{color:"red"}}>ABC</Navbar.Brand>
+        <Navbar.Brand href='#' style={{color:"green"}}>PQR</Navbar.Brand>
+        <Navbar.Brand href='#' style={{color:"blue"}}>RST</Navbar.Brand>
+      </Navbar>
+    </div>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<MyNavbar/> );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
